a5945ea203ec4dc61921a5106736c1185a61c0c2
Jordan Genovese <jordan.genovese@uvm.edu>
2020-04-14 14:31:55 -0400

    Added base views for scores and notification. Nav bar is somewhat styled.

---
+++ CKD Express/ViewControllers/PatientNotificationsView/PatientNotificationsViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PatientNotificationsViewController.swift
+//  CKD Express
+//
+//  Created by Jordan Genovese on 4/14/20.
+//  Copyright © 2020 Jordan Genovese. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PatientNotificationsViewController : UIViewController {
+    
+    
+}
---
+++ CKD Express/ViewControllers/PatientScoresView/PatientScoresViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PatientScoresViewController.swift
+//  CKD Express
+//
+//  Created by Jordan Genovese on 4/14/20.
+//  Copyright © 2020 Jordan Genovese. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PatientScoresViewController : UIViewController {
+    
+    
+}
