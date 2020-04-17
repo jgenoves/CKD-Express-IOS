759d66e1a0bacadf378ec68d96fffcb8cfa192da
hannahmeharg <mehargh@gmail.com>
2020-04-17 09:53:31 -0400

    added view controller. crashing

--- CKD Express/ViewControllers/PatientScoresView/PatientScoresViewController.swift
+++ CKD Express/ViewControllers/PatientScoresView/PatientScoresViewController.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Jordan Genovese on 4/14/20.
 //  Copyright © 2020 Jordan Genovese. All rights reserved.
-//
+// Hellooooooooo
 
 import Foundation
 import UIKit
@@ -17,9 +17,26 @@ class PatientScoresViewController : UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        patientData = self.delegate.activePatientUser
-        
+            
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 65
+    }
+    
+    override func tableView(_ tableView: UITableView,numberOfRowsInSection section: Int) -> Int {
+        return (patientData?.gfrScores.count)!
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Get a new or recycled cell
+         let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell",
+        for: indexPath)
+        // Set the text on the cell with the description of the item
+        // that is at the nth index of items, where n = row this cell
+        // will appear in on the tableview
+        let item = patientData?.gfrScores[indexPath.row]
+        cell.textLabel?.text? = "\(item!.score)"
+        cell.detailTextLabel?.text = item!.locationRecorded
+        return cell
     }
     
     
