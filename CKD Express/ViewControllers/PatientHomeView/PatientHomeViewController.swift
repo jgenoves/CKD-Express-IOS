4d413d7b3d39c164adc26bde7a353590515be449
Jordan Genovese <jordan.genovese@uvm.edu>
2020-04-23 17:11:24 -0400

    Fixed more files

--- CKD Express/ViewControllers/PatientHomeView/PatientHomeViewController.swift
+++ CKD Express/ViewControllers/PatientHomeView/PatientHomeViewController.swift
@@ -8,22 +8,27 @@
 
 import UIKit
 import Firebase
-
+        
 class PatientHomeViewController: UIViewController {
     
-    
     let delegate = UIApplication.shared.delegate as! AppDelegate
+    var patientData: Patient?
     
-    
+    @IBOutlet var userField: UILabel!
+    @IBOutlet var welcomeField: UILabel!
+    @IBOutlet var scoreField: UILabel!
+    @IBOutlet var stageField: UILabel!
+    @IBOutlet var baseField: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+        patientData = self.delegate.activePatientUser
+        self.userField.text = patientData?.firstName
+        self.scoreField.text = "\(patientData!.getMostRecentScore()!.score)"
+        self.stageField.text = patientData?.ckdStage
+        self.baseField.text = "\(patientData!.baseGFRScore)"
         
     }
-    
-    
-    
-    
+
 }
+
