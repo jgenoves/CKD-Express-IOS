8cc932e4ff4d95e4d16a2552acf7453659565587
Jordan Genovese <jordan.genovese@uvm.edu>
2020-04-24 15:05:49 -0400

    Styling changes to home page

--- CKD Express/ViewControllers/PatientHomeView/PatientHomeViewController.swift
+++ CKD Express/ViewControllers/PatientHomeView/PatientHomeViewController.swift
@@ -19,15 +19,36 @@ class PatientHomeViewController: UIViewController {
     @IBOutlet var scoreField: UILabel!
     @IBOutlet var stageField: UILabel!
     @IBOutlet var baseField: UILabel!
+    @IBOutlet var lastCheckupField: UILabel!
+    @IBOutlet var nephVisitWarning: UIImageView!
+    @IBOutlet var checkupVisitWarning: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         patientData = self.delegate.activePatientUser
-        self.userField.text = patientData?.firstName
+        self.userField.text = "\(patientData!.firstName + " " + patientData!.lastName)"
         self.scoreField.text = "\(patientData!.getMostRecentScore()!.score)"
         self.stageField.text = patientData?.ckdStage
         self.baseField.text = "\(patientData!.baseGFRScore)"
+        self.lastCheckupField.text = patientData?.getMostRecentScore()?.dateRecorded.dateValue().toString(dateFormat: "MM/dd/yyyy")
         
+        
+        if(patientData!.nephVisitNeeded) {
+            scoreField.textColor = UIColor.systemRed
+            scoreField.backgroundColor = UIColor.systemRed.withAlphaComponent( 0.1 )
+            scoreField.layer.cornerRadius = 5
+            scoreField.layer.masksToBounds = true
+            nephVisitWarning.isHidden = false
+        }
+        
+        patientData!.checkupNeeded = true
+        if(patientData!.checkupNeeded) {
+            lastCheckupField.textColor = UIColor.systemOrange
+            lastCheckupField.backgroundColor = UIColor.systemOrange.withAlphaComponent( 0.1 )
+            lastCheckupField.layer.cornerRadius = 5
+            lastCheckupField.layer.masksToBounds = true
+            checkupVisitWarning.isHidden = false
+        }
     }
 
 }
